0840d64a28e2460fe2fbe7be727b11484164ae3e
ami469 <66446277+ami469@users.noreply.github.com>
2025-07-17 18:12:41 +0700

    Update sangmata.py

--- X1puy/modules/basic/sangmata.py
+++ X1puy/modules/basic/sangmata.py
@@ -6,8 +6,8 @@ from pyrogram.errors import YouBlockedUser
 from pyrogram.types import *
 
 
-from Zaid.modules.help import add_command_help
-from Zaid.modules.basic.profile import extract_user
+from X1puy.modules.help import add_command_help
+from X1puy.modules.basic.profile import extract_user
 
 @Client.on_message(filters.command(["sg", "sa", "sangmata"], ".") & filters.me)
 async def sg(client: Client, message: Message):
