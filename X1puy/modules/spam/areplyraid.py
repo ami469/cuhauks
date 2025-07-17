f8923bb162c7b5d2b0ef4cb7a1b7f2f988b491ce
ami469 <66446277+ami469@users.noreply.github.com>
2025-07-17 18:45:15 +0700

    Update areplyraid.py

--- X1puy/modules/spam/areplyraid.py
+++ X1puy/modules/spam/areplyraid.py
@@ -6,13 +6,13 @@ from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from cache.data import *
-from Zaid.database.rraid import *
-from Zaid import SUDO_USER
+from X1puy.database.rraid import *
+from X1puy import SUDO_USER
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
 DEVS = int(1669178360)
-from Zaid.helper.PyroHelpers import get_ub_chats
-from Zaid.modules.basic.profile import extract_user, extract_user_and_reason
+from X1puy.helper.PyroHelpers import get_ub_chats
+from X1puy.modules.basic.profile import extract_user, extract_user_and_reason
 SUDO_USERS = SUDO_USER
 from .replyraid import RAIDS
 
