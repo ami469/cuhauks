de6d27def990bed4f8e4164024790dae0c395008
ami469 <66446277+ami469@users.noreply.github.com>
2025-07-17 17:53:21 +0700

    Update broadcast.py

--- X1puy/modules/basic/broadcast.py
+++ X1puy/modules/basic/broadcast.py
@@ -5,9 +5,9 @@ from pyrogram.types import Message
 from requests import get
 
 
-from Zaid import SUDO_USER
+from X1puy import SUDO_USER
 
-from Zaid.modules.help import add_command_help
+from X1puy.modules.help import add_command_help
 from cache.data import GROUP, VERIFIED_USERS
 NB = GROUP
 DEVS = VERIFIED_USERS
