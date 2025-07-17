090255f9dd15fa82b923b7c5ae786fe0074af611
ami469 <66446277+ami469@users.noreply.github.com>
2025-07-17 17:51:23 +0700

    Update afk.py

--- X1puy/modules/basic/afk.py
+++ X1puy/modules/basic/afk.py
@@ -5,8 +5,8 @@ import humanize
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from Zaid.helper.PyroHelpers import GetChatID, ReplyCheck
-from Zaid.modules.help import add_command_help
+from X1puy.helper.PyroHelpers import GetChatID, ReplyCheck
+from X1puy.modules.help import add_command_help
 
 AFK = False
 AFK_REASON = ""
