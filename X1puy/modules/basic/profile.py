8c02f1cdee3f52232aaf202009228a8a0ca2e66f
ami469 <66446277+ami469@users.noreply.github.com>
2025-07-17 18:11:32 +0700

    Update profile.py

--- X1puy/modules/basic/profile.py
+++ X1puy/modules/basic/profile.py
@@ -9,10 +9,10 @@ from time import time
 from pyrogram import Client, filters, enums
 from pyrogram.types import Message
 
-from Zaid import SUDO_USER
-from Zaid.helper.PyroHelpers import ReplyCheck
+from X1puy import SUDO_USER
+from X1puy.helper.PyroHelpers import ReplyCheck
 
-from Zaid.modules.help import add_command_help
+from X1puy.modules.help import add_command_help
 
 flood = {}
 profile_photo = "cache/pfp.jpg"
