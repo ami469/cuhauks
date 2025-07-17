da452d9d3c53eeb39fdbe1d949f08b99ffb5b2ff
ami469 <66446277+ami469@users.noreply.github.com>
2025-07-17 18:10:40 +0700

    Update ping.py

--- X1puy/modules/basic/ping.py
+++ X1puy/modules/basic/ping.py
@@ -6,11 +6,11 @@ from pyrogram import Client, filters
 from pyrogram.raw import functions
 from pyrogram.types import Message
 
-from Zaid import StartTime, app, SUDO_USER
-from Zaid.helper.PyroHelpers import SpeedConvert
-from Zaid.modules.bot.inline import get_readable_time
+from X1puy import StartTime, app, SUDO_USER
+from X1puy.helper.PyroHelpers import SpeedConvert
+from X1puy.modules.bot.inline import get_readable_time
 
-from Zaid.modules.help import add_command_help
+from X1puy.modules.help import add_command_help
 
 class WWW:
     SpeedTest = (
